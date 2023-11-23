ad5cda8477cd354406c129a3e56749d680e43f31
Asus <vaxa2320@gmail.com>
2023-11-23 21:01:10 +0300

    user

--- src/main/java/by/IT/model/entity/dibi/DbInMemory.java
+++ src/main/java/by/IT/model/entity/dibi/DbInMemory.java
@@ -2,10 +2,33 @@ package by.IT.model.entity.dibi;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DbInMemory {
-    private static  List<User> users = new ArrayList<>();
+    private static List<User> users = new ArrayList<>();
+
     static {
+
+        users.add(new User(1, "Vasia Pum", "vasia.pum@gmail.com"));
+        users.add(new User(1, "Ivan Ivanov", "van.vano@gmail.com"));
+        users.add(new User(1, "Petr Petrov", "petruha25@gmail.com"));
+
+
+    }
+
+
+    public static User findUserByName(String name) {
+        return users.stream()
+                .filter(it -> it.getFio().equalsIgnoreCase(name))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static List<User> findUsersByIds(int from, int to) {
+        return users.stream()
+                .filter(it -> it.getId() >= from && it.getId() <= to)
+                .collect(Collectors.toList());
 
     }
 }
+
---
+++ src/main/java/by/IT/model/entity/dibi/UserService.java
@@ -0,0 +1,31 @@
+package by.IT.model.entity.dibi;
+
+import java.util.*;
+
+import java.util.List
+import java.util.stream.Collectors;
+
+public class UserService {
+    public List<User> getUsersByCriteria(Map<String, String[]> params) {
+        List<User> users = new ArrayList<>();
+        String criteria = params.get("criteria")[0];
+        switch (criteria) {
+            case "name": {
+                String name = params.get("fio")[0];
+                users.add(DbInMemory.findUserByName(name));
+                users = users.stream()
+                        .filter(Objects::nonNull)
+                        .collect(Collectors.toList());
+                break;
+
+            }
+            case "id": {
+                int from = Integer.parseInt(params.get("fromId")[0]);
+                int to = Integer.parseInt(params.get("toId")[0]);
+              users = DbInMemory.findUsersByIds(from, to);
+              break;
+            }
+        }
+        return users;
+    }
+}
