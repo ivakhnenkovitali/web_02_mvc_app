b0aa981fb044742ff9261e39e6489eb6531790c2
Asus <vaxa2320@gmail.com>
2024-09-23 15:54:33 +0300

    user1_24_24

--- src/_Java_basics-master/src/main/java/by/_tests/MyClass.java
+++ src/_Java_basics-master/src/main/java/by/_tests/MyClass.java
@@ -3,7 +3,6 @@ public class MyClass {
         public static void firstMethod (){
             System.out.println("Это статический метод!");
         }
-
         public void secondMethod (){
             System.out.println("Это НЕ статический метод!");
         }
