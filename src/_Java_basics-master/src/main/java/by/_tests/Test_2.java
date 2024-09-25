d1822f4292d81d947a49ac7a51480bb45c92564a
Asus <vaxa2320@gmail.com>
2024-09-25 15:56:03 +0300

    user1_24_24

--- src/_Java_basics-master/src/main/java/by/_tests/Test_2.java
+++ src/_Java_basics-master/src/main/java/by/_tests/Test_2.java
@@ -9,7 +9,6 @@ public class Test_2 {
         long d = 1000;
         Object cd = c+d;
         System.out.println(cd.getClass().getName() +" value: " + cd);
-
         int a0 = 64;
         int a1 = 257;
         int a2 = 126;
@@ -19,8 +18,6 @@ public class Test_2 {
         byte b2 = (byte) a2;
         byte b3 = (byte) a3;
         System.out.println(b0+ " " + b1 + " " + b2 + " " + b3); //64 1 126 -127
-
-
        int [][] arr= {{1,2,3,4},{1,2,3,5}};
         System.out.println(arr[1].length);
 
