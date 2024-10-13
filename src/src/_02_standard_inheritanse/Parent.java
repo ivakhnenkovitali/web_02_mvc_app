98903f1b292a45cd3747e5502ae90278b05656b5
Asus <vaxa2320@gmail.com>
2024-10-13 18:01:02 +0300

    user1_24_24

--- src/src/_02_standard_inheritanse/Parent.java
+++ src/src/_02_standard_inheritanse/Parent.java
@@ -1,10 +1,7 @@
 package _02_standard_inheritanse;
-
 import java.io.Serializable;
-
 public class Parent{
     int parentField;
-
     public Parent() {
         System.out.println("parent constructor");
         parentField = 5;
