a77711a6e42bdbceb18f10562b274e767351b171
Asus <vaxa2320@gmail.com>
2024-10-14 18:02:03 +0300

    user1_24_24

--- src/src/_02_standard_inheritanse/Child.java
+++ src/src/_02_standard_inheritanse/Child.java
@@ -1,16 +1,12 @@
 package _02_standard_inheritanse;
-
 import java.io.Serializable;
 import java.util.StringJoiner;
-
 public class Child extends Parent implements Serializable {
     int childField;
-
     public Child(int childField) {
         System.out.println("child constructor");
         this.childField = childField;
     }
-
     @Override
     public String toString() {
         return new StringJoiner(", ", Child.class.getSimpleName() + "[", "]")
