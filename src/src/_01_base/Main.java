366aa1a69933910bc01a659ea0ed354f886d15c6
Asus <vaxa2320@gmail.com>
2024-10-19 18:04:56 +0300

    user1_24_24

--- src/src/_01_base/Main.java
+++ src/src/_01_base/Main.java
@@ -4,7 +4,6 @@ import java.util.stream.Collectors;
 public class Main {
     public static final String INPUT = "src/_01_base/in.file";
     public static final String OUTPUT = "src/_01_base/out.txt";
-
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new FileReader(INPUT));
              FileOutputStream os = new FileOutputStream(OUTPUT)) {
